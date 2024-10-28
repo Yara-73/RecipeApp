344cfb139e6e246c01af0d438f130bcb39045085
Yara-73 <krrm.73li@gmail.com>
2024-10-28 10:30:38 +0300

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Yara Alshammari on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeAppTests/RecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppTests.swift
+//  RecipeAppTests
+//
+//  Created by Yara Alshammari on 25/04/1446 AH.
+//
+
+import Testing
+@testable import RecipeApp
+
+struct RecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
